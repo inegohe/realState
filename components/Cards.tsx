3ee35241e20679fbd572fee9aca2335ace00915b
inegohe <ikhosler@gmail.com>
2025-01-24 07:44:57 +0300

    prepare eas cli

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -37,7 +37,7 @@ export const FeaturedCard = ({ item: {image, rating, name, address, price}, onPr
 
         <View className="flex flex-row items-center justify-between w-full">
           <Text className="text-xl font-rubik-extrabold text-white">
-            {price}
+            ${price}
           </Text>
 
           <Image source={icons.heart} className="size-5" />
@@ -71,7 +71,7 @@ export const Card = ({ item: {image, rating, name, address, price}, onPress }: P
 
         <View className="flex flex-row items-center justify-between mt-2">
           <Text className="text-base font-rubik-bold text-primary-300">
-            {price}
+            ${price}
           </Text>
 
           <Image source={icons.heart} className="w-5 h-5 mt-2" tintColor='#191d3'/>
